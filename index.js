6e190b32a7df3d9f14ed6027ddecd70089e944b3
Victor Truong <me@ifvictr.com>
2019-03-16 03:29:10 -0700

    Update code style

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-"use strict";
+'use strict'
 
-const Promise = require("bluebird");
-const BaseStorage = require("ghost-storage-base");
-const GitHub = require("github");
-const fs = require("fs");
-const path = require("path");
+const Promise = require('bluebird')
+const BaseStorage = require('ghost-storage-base')
+const GitHub = require('github')
+const fs = require('fs')
+const path = require('path')
 
-const buildUrl = require("build-url");
-const isUrl = require("is-url");
-const readFile = Promise.promisify(fs.readFile);
-const removeLeadingSlash = require("remove-leading-slash");
-const request = Promise.promisify(require("request"));
+const buildUrl = require('build-url')
+const isUrl = require('is-url')
+const readFile = Promise.promisify(fs.readFile)
+const removeLeadingSlash = require('remove-leading-slash')
+const request = Promise.promisify(require('request'))
 
 class GitHubStorage extends BaseStorage {
     constructor(config) {
-        super();
+        super()
 
-        this.client = new GitHub();
-        this.config = config;
-        config.branch = config.branch || "master";
-        config.destination = config.destination || "";
+        this.client = new GitHub()
+        this.config = config
+        config.branch = config.branch || 'master'
+        config.destination = config.destination || ''
 
         this.client.authenticate({
             type: config.type,
             username: config.user,
             password: config.password,
             token: config.token,
-        });
+        })
     }
 
     delete() {
         // TODO: Find a way to get the blob SHA of the target file
-        return Promise.reject("Not implemented");
+        return Promise.reject('Not implemented')
     }
 
     exists(filename, targetDir) {
-        const filepath = path.join(targetDir || this.getTargetDir(), filename);
+        const filepath = path.join(targetDir || this.getTargetDir(), filename)
 
         return request(this.getUrl(filepath))
             .then(res => res.statusCode === 200)
-            .catch(() => false);
+            .catch(() => false)
     }
 
     read(options) {
@@ -47,40 +47,42 @@ class GitHubStorage extends BaseStorage {
     }
 
     save(file, targetDir) {
-        const {branch, repo, user} = this.config;
-        const dir = targetDir || this.getTargetDir();
+        const { branch, repo, user } = this.config
+        const dir = targetDir || this.getTargetDir()
 
-        return Promise.join(this.getUniqueFileName(file, dir), readFile(file.path, "base64"), (filename, data) => {
+        return Promise.join(this.getUniqueFileName(file, dir), readFile(file.path, 'base64'), (filename, data) => {
             return this.client.repos.createFile({
                 owner: user,
                 repo: repo,
                 branch: branch,
-                message: "Add new image",
+                message: 'Add new image',
                 path: this.getFilepath(filename),
                 content: data
-            });
+            })
         })
             .then(res => res.data.content.download_url)
-            .catch(Promise.reject);
+            .catch(Promise.reject)
     }
 
     serve() {
         return (req, res, next) => {
-            next();
-        };
+            next()
+        }
     }
 
     getUrl(filename) {
-        const {baseUrl, branch, repo, user} = this.config;
-        let url = isUrl(baseUrl) ? baseUrl : `https://raw.githubusercontent.com/${user}/${repo}/${branch}`;
-        url = buildUrl(url, {path: this.getFilepath(filename)});
+        const { baseUrl, branch, repo, user } = this.config
+        let url = isUrl(baseUrl)
+            ? baseUrl
+            : `https://raw.githubusercontent.com/${user}/${repo}/${branch}`
+        url = buildUrl(url, { path: this.getFilepath(filename) })
 
-        return url;
+        return url
     }
 
     getFilepath(filename) {
-        return removeLeadingSlash(path.join(this.config.destination, filename));
+        return removeLeadingSlash(path.join(this.config.destination, filename))
     }
 }
 
-module.exports = GitHubStorage;
+module.exports = GitHubStorage
