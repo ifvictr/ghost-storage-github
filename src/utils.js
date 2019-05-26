79f086c3e4ae0a8a78e74f8f19ee672cbe26c2da
Victor Truong <me@ifvictr.com>
2019-05-25 20:45:33 -0700

    Handle both http and https URLs, closes #20

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,20 @@
 import Promise from 'bluebird'
 import http from 'http'
+import https from 'https'
+import { URL } from 'url'
+
+const getProtocolAdapter = inputUrl => {
+    const url = new URL(inputUrl)
+    const adapters = {
+        'http:': http,
+        'https:': https
+    }
+
+    return adapters[url.protocol]
+}
 
 export const isWorkingUrl = url => new Promise(resolve => {
-    const req = http.request(url, { method: 'HEAD' }, res => {
+    const req = getProtocolAdapter(url).request(url, { method: 'HEAD' }, res => {
         resolve(res.statusCode >= 200 && res.statusCode < 400)
     })
     req.end()
